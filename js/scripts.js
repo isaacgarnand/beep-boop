a023d57470a73f5ddeb931dee81be9b744f25f17
isaacgarnand <isaac.garnand@gmail.com>
2020-01-24 09:59:08 -0800

    made bad logic - needs to populate array first then check that array with .includes

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,20 @@ $(document).ready(function() {
     var outputArray = [];
 
     for (var i = 0; i < userInput; i++) {
-      outputArray.push(i);
+      if (outputArray[i].includes(3)) {
+        outputArray.push("I'm sorry Dave, I can't do that.");
+      }
+      else if (outputArray[i].includes(2)) {
+        outputArray.push("boop!");
+      }
+      else if (outputArray[i].includes(1)) {
+        outputArray.push("beep!");
+      }
+      else {
+        outputArray.push(i);
+      }
     }
-
+    
     $("#output").show();
     $("#output").text(outputArray + "," + userInput);
 

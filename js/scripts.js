f57fdb30eacd3d9488ff6f6825e5bc8c4e6d0613
isaacgarnand <isaac.garnand@gmail.com>
2020-01-24 09:43:39 -0800

    added js logic for first two specs (returns array up to inputnumber)

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,15 @@ $(document).ready(function() {
   $("form#input").submit(function(event) {
     event.preventDefault();
 
-    
+    var userInput = parseInt($("input#usernumber").val());
+    var outputArray = [];
+
+    for (var i = 0; i < userInput; i++) {
+      outputArray.push(i);
+    }
+
+    $("#output").show();
+    $("#output").text(outputArray + "," + userInput);
 
   });
 });

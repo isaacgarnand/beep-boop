5a8c7f9256cea1ea7f977ca605273d7e1c85b145
isaacgarnand <isaac.garnand@gmail.com>
2020-01-24 12:06:34 -0800

    added .test & regex variables to logic, works for all specs now(except none numbers)

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,23 @@ $(document).ready(function() {
     var userInput = parseInt($("input#usernumber").val());
     var intArray = [];
     var outputArray = [];
+    
+    var reg1 = /1/
+    var reg2 = /2/
+    var reg3 = /3/
 
     for (var i = 0; i < userInput; i++) {
       intArray.push(i);
     }
 
     for (var i = 0; i <= intArray.length; i++) {
-      if (intArray[i] === 3) {
+      if (reg3.test(intArray[i]) === true) {
         outputArray.push("I'm sorry Dave, I can't do that.");
       }
-      else if (intArray[i] === 2) {
+      else if (reg2.test(intArray[i]) === true) {
         outputArray.push("boop!");
       }
-      else if (intArray[i] === 1) {
+      else if (reg1.test(intArray[i]) === true) {
         outputArray.push("beep!");
       }
       else {
